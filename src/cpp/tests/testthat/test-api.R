531430830a393ae7c85030d9870709bb56b8c918
JesseAlderliesten <59017087+JesseAlderliesten@users.noreply.github.com>
2026-05-05 19:34:44 +0200

    Fix uninformative error in `rstudio::sourceMarkers()` when `marker$type` is not a length-one vector (#17541)
    * Bugfix to get informative error if `marker$type` is non-NULL zerolength object instead of `missing value where TRUE/FALSE needed`.
    
    * Test that invalid marker type generates informative error.
    
    * Also catch multi-valued marker$type like c("warning", "error").
    
    * Use `file.path()` to construct portable file paths. Add comment about behaviour with valid `type`.
    
    * Use toString() to generate error message for length(marker$type) > 1L.
    
    * Test error message for sourceMarkers() with length(marker$type) > 1L.
    
    * Fix uninformative error in `rstudio::sourceMarkers()` when `marker$type` is not a length-one vector.
    
    * Call .rs.api.sourceMarkers() directly instead of indirectly through rstudioapi::sourceMarkers().

--- src/cpp/tests/testthat/test-api.R
+++ src/cpp/tests/testthat/test-api.R
@@ -16,22 +16,30 @@
 context("rstudioapi")
 test_that("invalid marker type generates informative error", {
    NULL_type <- list(type = NULL,
-                     file = ".\\R\\fake_script.R",
+                     # 'file' would have to point to an actual R script to
+                     # prevent the error 'The system cannot find the path
+                     # specified' in normalizePath() in case of a valid 'type'.
+                     file = file.path(".", "R", "fake_script.R"),
                      line = 1L,
                      column = 1L,
                      message = "Some message")
-   zerolength_type <- NULL_type
-   zerolength_type[["type"]] <- character(0)
+   char0_type <- NULL_type
+   char0_type[["type"]] <- character(0)
+   vector_type <- NULL_type
+   vector_type[["type"]] <- c("warning", "error")
    
    expect_error(
-      .rs.api.sourceMarkers(name = "with_NULL_type",
-                            markers = list(NULL_type)),
+      .rs.api.sourceMarkers(name = "with_NULL_type", markers = list(NULL_type)),
       regexp = "Invalid marker type", fixed = TRUE
    )
-
+   
    expect_error(
-      .rs.api.sourceMarkers(name = "with_zerolength_type",
-                            markers = list(zerolength_type)),
+      .rs.api.sourceMarkers(name = "with_char0_type", markers = list(char0_type)),
+      regexp = "Invalid marker type", fixed = TRUE
+   )
+   
+   expect_error(
+      .rs.api.sourceMarkers(name = "with_vector_type", markers = list(vector_type)),
       regexp = "Invalid marker type", fixed = TRUE
    )
 })
